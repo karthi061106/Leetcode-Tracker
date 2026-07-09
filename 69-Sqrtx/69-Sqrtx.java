3dbf83621fa6f505a62d12a64893f0b77cb33cf9
karthi061106 <24243043@drngpit.ac.in>
2026-07-09 15:08:40 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:08:40 PM
+class Solution {
+    public int mySqrt(int x) {
+        if (x>=0){
+            return (int) Math.sqrt(x);
+        }else{
+            return 0;
+        }
+       
+         
+         
+       
+         
+    }
+
+}

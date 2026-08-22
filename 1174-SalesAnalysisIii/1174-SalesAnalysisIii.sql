1f011930282eba15cf7a8a8656df13b9e0d3fc32
karthi061106 <24243043@drngpit.ac.in>
2026-08-22 16:09:40 +0530

    Create 1174-SalesAnalysisIii

---
+++ 1174-SalesAnalysisIii/1174-SalesAnalysisIii.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/22/2026, 4:09:47 PM
+SELECT
+    p.product_id,
+    p.product_name
+FROM Product p
+JOIN Sales s
+ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01'
+   AND MAX(s.sale_date) <= '2019-03-31';

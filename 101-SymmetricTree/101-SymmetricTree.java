5593642138b83fc507c4cb52a43f32e9fa548c01
karthi061106 <24243043@drngpit.ac.in>
2026-08-22 16:13:17 +0530

    Create 101-SymmetricTree

---
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -0,0 +1,18 @@
+// Last updated: 8/22/2026, 4:13:24 PM
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) {
+            return true;
+        }
+        
+        if (n1 == null || n2 == null) {
+            return false;
+        }
+        
+        return n1.val == n2.val && isMirror(n1.left, n2.right) && isMirror(n1.right, n2.left);
+    }
+}
